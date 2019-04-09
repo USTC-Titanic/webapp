12f56be6ba67954359eea6af7f596ff1bbeecd06
FrozenMap <ljjdbd123@hotmail.com>
2019-04-09 14:48:22 +0000

    update news module

--- news.py
+++ news.py
@@ -3,56 +3,137 @@ import requests, json
 import os.path
 from time import time
 
+filename_36kr = 'db/36kr.json'
+# url_36kr = 'https://36kr.com/api/newsflash?&per_page=20'
+url_36kr = 'https://36kr.com/api/newsflash'
+
 class Spider(object):
-	filename_36kr = 'db/36kr.json'
-	url_36kr = 'https://36kr.com/api/newsflash?&per_page=20'
-	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
+	def get_news(self, src="36kr"):
+		if src == "36kr":
+			if self.news_expire(filename_36kr) == True:
+				return self.update_36kr_news()
+			else:
+				return self.load_news(filename_36kr)
+		else:
+			return '[{"title":"", "content":"", "time":""}]'
 
-	def update_news(self):
-		last_modified_36kr = os.path.gettime(filename_36kr)
-		now = time()
-		# 如果超过了 10 分钟, 就更新新闻文件
-		expires_36kr = now - last_modified_36kr > 600
-		if expires_36kr:
-			self.get_news(url_36kr)
-
-	def get_news(self, url=url_36kr, filename='36kr'):
+	def update_36kr_news(self):
 		try:
-			sess = requests.Session()
-			resp = sess.get(url, headers=headers, )
-			text = resp.text
-			# TODO 不保存无效信息
-			# 只保存 title, description, time, link
-			# news_filter
-			with open('db/%s.json' % filename, 'w') as f:
-				f.write(text)
+			headers = {
+				'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+				'Accept-Encoding': 'gzip, deflate',
+				'Accept-Language': 'zh-CN,zh;q=0.9',
+				'Host': '36kr.com',
+				'Connection': 'keep-alive',
+				'Cache-Control': 'no-cache',
+				'Pragma': 'no-cache',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+			}
+			json_resp = requests.Session().get(url_36kr, headers=headers).json()['data']['items']
+			news_list = []
+			for item in json_resp:
+				news = {}
+				# 只保存 title, content, time
+				news['title'] = item['title']
+				news['content'] = item['description']
+				news['time'] = item['created_at']
+				news_list.append(news)
+			news_list = json.dumps(news_list, ensure_ascii=False)
+			with open(filename_36kr, 'w', encoding="utf-8") as f:
+				f.write(news_list)
+			return news_list
 		except Exception as e:
 			pass
 
-	def get_json_news(self, ):
+	def store_jiqizhixin_news(self, json_resp={}):
+		'''
+		TODO
+		如何获得 X-CSRF-Token
+		或者, 使用 Selenium + phantomjs
+		'''
+		headers = {
+			'Accept': '*/*',
+			'Accept-Encoding': 'gzip, deflate, br',
+			'Accept-Language': 'zh-CN,zh;q=0.9',
+			'Connection': 'keep-alive',
+			'Content-Length': '525',
+			'Cache-Control': 'no-cache',
+			'content-type': 'application/json',
+			'Host': 'www.jiqizhixin.com',
+			'Origin': 'https://www.jiqizhixin.com',
+			'Referer': 'https://www.jiqizhixin.com/dailies',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36',
+		}
+		post_data = {
+			"operationName":"Dailies",
+			"variables":{"cursor":""},
+			"query":'\
+"query Dailies($cursor: String) {\
+  dailies(first: 10, after: $cursor) {\
+    edges {\
+      node {\
+        ...DailyInfo\
+        __typename\
+      }\
+      __typename\
+    }\
+    pageInfo {\
+      ...PageInfo\
+      __typename\
+    }\
+    __typename\
+  }\
+}\
+\
+fragment DailyInfo on Daily {\
+  id\
+  title\
+  content\
+  likes_count\
+  url\
+  created_at\
+  path\
+  __typename\
+}\
+\
+fragment PageInfo on PageInfo {\
+  endCursor\
+  hasNextPage\
+  __typename\
+}\
+"\
+'
+		}
+		pass
+
+	def news_expire(self, filename):
+		'''
+		如果超过了 10 分钟, 则判定为过期, 需要更新新闻文件
+		'''
 		try:
-			with open('db/%s.json' % filename_36kr, 'r') as f:
-				content = f.read()
-			news = {'36kr': json.loads(content)}
-			return news
-		except Exception as e:
-			raise
+			last_modified = os.path.getmtime(filename)
+			now = time()
+			expires = now - last_modified > 600
+			return expires
+		except FileNotFoundError as e:
+			return False
 
-	def test1(self):
-		self.update_news()
+	def load_news(self, filename):
+		try:
+			with open(filename, 'r') as f:
+				news_list = f.read()
+			return news_list
+		except FileNotFoundError as e:
+			return ""
 
-	def test2(self):
-		d = self.get_json_news()
-		print(d['data']['items'][0]['title'])
 
 spider = Spider()
 
 class NewsHandler(PageHandler):
-	filename = 'news/news.html'
 	def get(self):
-		spider.update_news()
+		filename = 'news/news.html'
 		if self.get_args('q') == 'json':
-			return self.render(spider.get_json_news())
-		else:
-			return self.render_file(filename)
+			src = self.get_args('src')
+			return self.render( spider.get_news(src) )
 
+		return self.render_file(filename)
